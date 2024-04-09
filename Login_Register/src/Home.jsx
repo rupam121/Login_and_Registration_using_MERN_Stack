5f95183dab6ab591d08256e5a285e4c3d90a1ac0
RUPAM GIRI <rupamgiri4321@gmail.com>
2024-04-09 23:18:50 +0530

    Login and Registration using MERN Stack

---
+++ Login_Register/src/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <h2>Home Componenet</h2>
+    </div>
+  );
+}
+
+export default Home;
